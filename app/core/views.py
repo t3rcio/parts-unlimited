64471c1a06842b63237866436ecdebc526b3b476
Tercio <t3rcio@gmail.com>
2025-04-18 21:44:37 -0300

    Enable DjangoNinja API

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+from ninja import NinjaAPI
+
+from core.models import Part
+
+import json
+import sys
+import traceback
+import logging
+
+logging.basicConfig(
+    filename = settings.LOG_FILENAME
+)
+
+api = NinjaAPI(
+    openapi_extra={
+        'info': {
+            'termsOfService' : 'https://github.com/t3rcio/parts-unlimited'
+        },
+        'title': 'API Server - Parts Unlimited Project',
+        'description': 'API DEMO - Parts Unlimited',
+    }
+)
+
+@api.get('/parts')
+def parts(request):
+    parts = Part.objects.all()
+    parts_collection = [
+        p.to_dict() for p in parts    
+    ]
+    response = json.dumps(parts_collection)
+    return JsonResponse(response, safe=False)
+
--- app/parts_unlimited/urls.py
+++ app/parts_unlimited/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ninja import NinjaAPI
+from core.views import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
